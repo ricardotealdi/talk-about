a4ab02f80668ca5c760cd7224da81a9f1ccd41c4
Ricardo Tealdi <ricardo.tealdi@gmail.com>
2011-10-14 14:47:21 -0300

    created subject model

---
+++ src/main/java/br/com/tealdi/talkabout/model/Subject.java
@@ -0,0 +1,26 @@
+package br.com.tealdi.talkabout.model;
+
+public class Subject {
+	private String name;
+	private int id;
+
+	public Subject(String name) {
+		this.setName(name);
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getId() {
+		return id;
+	}
+}
