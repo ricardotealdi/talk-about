de6ba88f75b72f38a4f1f9f6ca25b9480068a3d4
Ricardo Tealdi <ricardo.tealdi@gmail.com>
2011-10-13 22:38:33 -0300

    created vraptor structure project

---
+++ src/main/java/br/com/tealdi/talkabout/controller/IndexController.java
@@ -0,0 +1,21 @@
+package br.com.tealdi.talkabout.controller;
+
+import br.com.caelum.vraptor.Path;
+import br.com.caelum.vraptor.Resource;
+import br.com.caelum.vraptor.Result;
+
+@Resource
+public class IndexController {
+
+	private final Result result;
+
+	public IndexController(Result result) {
+		this.result = result;
+	}
+
+	@Path("/")
+	public void index() {
+		result.include("variable", "VRaptor!");
+	}
+
+}
